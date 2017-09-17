af2e05a36f0544ed6cf086402c537929f9e67122
Rokney <rantahu@gmail.com>
2017-09-17 03:11:22 -0400

    fixed typos

--- django_apps/checker/api/urls.py
+++ django_apps/checker/api/urls.py
@@ -6,8 +6,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
-    url(r'^api/listings/create/$', views.ListingCreate.as_view()),
-    url(r'^api/listings/delte/(?P<pk>[0-9]+)/$', views.ListingDelete.as_view()),
-    url(r'^api/listings/(?P<pk>[0-9]+)/$', views.ListingDetial.as_view()),
-    url(r'^api/listings/$', views.ListingList.as_view()),
+    url(r'^api/checker/create/$', views.LinkCreate.as_view()),
+    url(r'^api/checker/delete/(?P<pk>[0-9]+)/$', views.LinkDelete.as_view()),
+    url(r'^api/checker/(?P<pk>[0-9]+)/$', views.LinkDetail.as_view()),
+    url(r'^api/checker/$', views.LinkList.as_view()),
 ]
